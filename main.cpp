e78d384fceffd3df38f184ab2e48523bc7e8e12c
UrielTorres02 <hernando.uri23@gmail.com>
2023-02-10 12:37:03 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+
+
+struct nodo{
+    // string con el nombre del vendedor
+    // array dinamico con el nombre de los clientes
+};
+
+// crear funcion para eliminar y buscar
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

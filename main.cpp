9b02d84756df7072d34d786f088efe456d053766
UrielTorres02 <hernando.uri23@gmail.com>
2023-02-10 12:44:05 -0600

    segundo commit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@
 struct nodo{
     // string con el nombre del vendedor
     // array dinamico con el nombre de los clientes
+    struct nodo *sigiente;
 };
+
+typedef struct nodo *NODO;
+typedef struct NODO *PNODO;
 
 // crear funcion para eliminar y buscar
 
